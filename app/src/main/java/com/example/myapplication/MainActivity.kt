89234b1e2cc5838c8b028ba8cfd487eaffe494a2
Patriaji Ibrahim Maulana <patriajimaulana@gmail.com>
2019-03-20 18:34:27 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log
+import android.util.Log.d
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        myButton.setOnClickListener {
+
+            Log.i("MainActivity", "button was clicked")
+            Toast.makeText(this, "Button was Cicked", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
